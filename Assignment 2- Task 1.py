a6c29d3533a62cf64c1dbd250cd85e00c06cdcfc
Shivam Kumar Mishra <137914957+shivam-al@users.noreply.github.com>
2026-02-13 18:15:45 +0530

    Add files via upload

---
+++ Assignment 2- Task 1.py
@@ -0,0 +1,5 @@
+p= int(input("Enter a Number: "))
+if p%2==0:
+    print(f" {p}  is an even number.")
+else:
+    print(f"{p} is an odd number.")
---
+++ Assignment 2- Task2.py
@@ -0,0 +1,5 @@
+total = 0
+
+for x in range(1,51):
+    total = total + x
+print(f"The Sum of the integers from 1 to 50 is : {total}")
